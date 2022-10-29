d9dded07efa4e76106c43a51b6380d66c2fe79de
Emmanuel Robles <rore021226@gs.utm.mx>
2022-10-29 18:47:33 -0500

    Creacion de la funcion crearTabla, busquedaPareser y el archivo de tablaPalabrasReservadas

--- lexico/src/main.py
+++ lexico/src/main.py
@@ -1,10 +1,30 @@
 import sys
 
 
+def creaTabla(archivo):
+    a = {}
+    with open(archivo) as f:
+        for line in f:
+            (k, v) = line.split(',')
+            a[str(k)] = int(v)
+    return a
+
+def busquedaPaReser(palabra, tablaPalabrasReservadas):
+    return tablaPalabrasReservadas[palabra]
 
 if __name__ == '__main__':
     """
 
     :return: valor de salida del programa principal
     """
-    archivoNombre = sys.argv[1]
+    archivo = "../entradas/tablaPalabrasReservadas.txt"
+
+    archivoNombre = sys.argv[1]            #Para la lectura por consola del archivo
+    tablaPalabrasReservadas = creaTabla(archivo)
+    print(tablaPalabrasReservadas)
+    print(busquedaPaReser("else", tablaPalabrasReservadas))
+
+
+
+
+
