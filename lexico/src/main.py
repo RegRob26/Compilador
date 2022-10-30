d00a39042393779e86abde06e289a4e1ad1af1cb
Emmanuel Robles <rore021226@gs.utm.mx>
2022-10-29 20:00:14 -0500

    Creacion de los archivos de entrada y salida

--- lexico/src/main.py
+++ lexico/src/main.py
@@ -1,26 +1,33 @@
 import sys
-
-
 def creaTabla(archivo):
-    a = {}
+    """
+
+    :param archivo:
+    :return:
+    """
+    diccionario = {}
     with open(archivo) as f:
         for line in f:
             (k, v) = line.split(',')
-            a[str(k)] = int(v)
-    return a
+            diccionario[str(k)] = int(v)
+    return diccionario
 
 def busquedaPaReser(palabra, tablaPalabrasReservadas):
+    """
+
+    :param palabra: Contiene la palabra a buscar dentro del diccionario.
+    :param tablaPalabrasReservadas: Es el diccionario de las palabras reservadas en el que se buscara la palabra.
+    :return: el valor de la palabra(token) como entero o None en caso de estar dentro del diccionario.
+    """
     return tablaPalabrasReservadas[palabra]
 
 if __name__ == '__main__':
-    """
-
-    :return: valor de salida del programa principal
-    """
     archivo = "../entradas/tablaPalabrasReservadas.txt"
 
     archivoNombre = sys.argv[1]            #Para la lectura por consola del archivo
     tablaPalabrasReservadas = creaTabla(archivo)
+
+
     print(tablaPalabrasReservadas)
     print(busquedaPaReser("else", tablaPalabrasReservadas))
 
